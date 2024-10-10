e908a92b0f1bc0767c8e72e943bca3205d219d00
FanYuan <1569297330@qq.com>
2024-10-10 22:18:06 +0800

    [feat]add CompilePrinciples File

---
+++ WindowsTheory/Second/FireAlarmApp/ClassLib.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FireAlarmApp
+{
+    public class FireAlarmEventArgs : EventArgs
+    {
+        public string Location { get; set; }
+        public string Severity { get; set; }
+
+        public FireAlarmEventArgs(string location, string severity)
+        {
+            Location = location;
+            Severity = severity;
+        }
+    }
+
+    public class FireAlarmTrigger
+    {
+        // 使用事件和委托来触发事件
+        public event EventHandler<FireAlarmEventArgs> FireAlarmTriggered;
+
+        // 触发火警事件
+        public void TriggerFireAlarm(string location, string severity)
+        {
+            // 创建事件参数
+            FireAlarmEventArgs args = new FireAlarmEventArgs(location, severity);
+
+            // 如果事件有响应者，则触发事件
+            FireAlarmTriggered?.Invoke(this, args);
+        }
+    }
+
+    public class FireAlarmHandler
+    {
+        // 处理火警事件的函数1
+        public void HandleFireAlarmLevel1(object sender, FireAlarmEventArgs e)
+        {
+            MessageBox.Show($"火警 1 - 地点: {e.Location}, 严重性: {e.Severity}");
+        }
+
+        // 处理火警事件的函数2
+        public void HandleFireAlarmLevel2(object sender, FireAlarmEventArgs e)
+        {
+            MessageBox.Show($"火警 2 - 地点: {e.Location}, 严重性: {e.Severity}");
+        }
+    }
+}
---
+++ WindowsTheory/Second/FireAlarmApp/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FireAlarmApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FireAlarmTrigger fireAlarmTrigger;
+        private FireAlarmHandler fireAlarmHandler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 初始化事件触发者和事件响应者
+            fireAlarmTrigger = new FireAlarmTrigger();
+            fireAlarmHandler = new FireAlarmHandler();
+
+            // 设置按钮点击事件
+            btnBind1.Click += (sender, e) => Bind1();
+            btnBind2.Click += (sender, e) => Bind2();
+            btnUnbind1.Click += (sender, e) => Unbind1();
+            btnUnbind2.Click += (sender, e) => Unbind2();
+            btnTrigger1.Click += (sender, e) => TriggerEvent1();
+            
+        }
+
+        // 动态绑定事件响应者1
+        private void Bind1()
+        {
+            btnBind1.Background = new SolidColorBrush(Colors.Red);
+
+
+            fireAlarmTrigger.FireAlarmTriggered += fireAlarmHandler.HandleFireAlarmLevel1;
+
+
+            Task.Delay(500).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => btnBind1.Background = new SolidColorBrush(Color.FromRgb(45, 79, 107)));
+            });
+        }
+
+        // 动态绑定事件响应者2
+        private void Bind2()
+        {
+            btnBind2.Background = new SolidColorBrush(Colors.Red);
+
+            fireAlarmTrigger.FireAlarmTriggered += fireAlarmHandler.HandleFireAlarmLevel2;
+
+
+
+            Task.Delay(500).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => btnBind2.Background = new SolidColorBrush(Color.FromRgb(45, 79, 107)));
+            });
+        }
+
+        // 动态解绑事件响应者1
+        private void Unbind1()
+        {
+            btnUnbind1.Background = new SolidColorBrush(Colors.Red);
+
+
+            fireAlarmTrigger.FireAlarmTriggered -= fireAlarmHandler.HandleFireAlarmLevel1;
+
+
+            Task.Delay(500).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => btnUnbind1.Background = new SolidColorBrush(Color.FromRgb(45, 79, 107)));
+            });
+        }
+
+        // 动态解绑事件响应者2
+        private void Unbind2()
+        {
+            btnUnbind2.Background = new SolidColorBrush(Colors.Red);
+
+            
+            fireAlarmTrigger.FireAlarmTriggered -= fireAlarmHandler.HandleFireAlarmLevel2;
+
+
+            Task.Delay(500).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => btnUnbind2.Background = new SolidColorBrush(Color.FromRgb(45, 79, 107)));
+            });
+        }
+
+        // 触发火警事件
+        private void TriggerEvent1()
+        {
+            
+            btnTrigger1.Background = new SolidColorBrush(Colors.Red);
+
+            fireAlarmTrigger.TriggerFireAlarm("厨房", "严重");
+
+            
+            Task.Delay(500).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => btnTrigger1.Background = new SolidColorBrush(Color.FromRgb(45, 79, 107)));
+            });
+        }
+        
+    }
+}
